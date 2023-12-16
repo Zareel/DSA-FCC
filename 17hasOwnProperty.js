8bf7e098c2adfbdcf519b56bc29486db14cc3147
Zareel Kalam <zareelbwd@gmail.com>
2023-12-16 12:08:34 +0530

    completed more tasks

---
+++ 17hasOwnProperty.js
@@ -0,0 +1,38 @@
+//* Check if an Object has a Property
+//? Finish writing the function so that it returns true if the object passed to it contains all four names, Alan, Jeff, Sarah and Ryan and returns false otherwise.
+let users = {
+  Alan: {
+    age: 27,
+    online: true,
+  },
+  Jeff: {
+    age: 32,
+    online: true,
+  },
+  Sarah: {
+    age: 48,
+    online: true,
+  },
+  Ryan: {
+    age: 19,
+    online: true,
+  },
+};
+
+function isEveryoneHere(userObj) {
+  // Only change code below this line
+  if (
+    userObj.hasOwnProperty("Alan") &&
+    userObj.hasOwnProperty("Jeff") &&
+    userObj.hasOwnProperty("Sarah") &&
+    userObj.hasOwnProperty("Ryan")
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+
+  // Only change code above this line
+}
+
+console.log(isEveryoneHere(users));
